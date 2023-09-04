a833865c2c26ea1436e4e3b5a6ff5c991b9bd642
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2023-09-04 19:17:27 +0200

    Adding entries to the journal page

--- mood/app/(dashboard)/journal/page.tsx
+++ mood/app/(dashboard)/journal/page.tsx
@@ -1,10 +1,23 @@
+import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
 async function getEntries() {
-  //
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
 }
 
-const JournalPage = () => {
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log(entries)
   return <div>journal</div>
 }
 
--- mood/utils/auth.ts
+++ mood/utils/auth.ts
@@ -1,15 +1,13 @@
 import { auth } from '@clerk/nextjs'
 import { prisma } from './db'
 
-export async function getUserByClerkId({ includes, select }) {
+export async function getUserByClerkId() {
   const { userId } = await auth()
 
   const user = await prisma.user.findUniqueOrThrow({
     where: {
       clerkId: userId,
     },
-    select,
-    includes,
   })
 
   return user
