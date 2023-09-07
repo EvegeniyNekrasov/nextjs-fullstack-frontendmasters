e0b69efa74ad07307a2e73c4e330b09aa81cef80
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2023-09-07 19:52:21 +0200

    Adding functionality to editor and adding autosave

--- mood/utils/api.ts
+++ mood/utils/api.ts
@@ -2,6 +2,22 @@ const createUrl = (path: any) => {
   return window.location.origin + path
 }
 
+export const updateEntry = async (id, content) => {
+  const res = await fetch(
+    new Request(createUrl(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    })
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  } else {
+    throw new Error('Something went wrong on API server!')
+  }
+}
+
 export const createNewEntry = async () => {
   const res = await fetch(
     new Request(createUrl('/api/journal'), {
