6e8bea03025c604ea7fea768e800e8e707971bf9
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2023-09-09 18:34:53 +0200

    Adding logic and answer from openai to search bar

--- mood/components/Question.tsx
+++ mood/components/Question.tsx
@@ -1,31 +1,46 @@
 'use client'
 
+import { askQuestion } from '@/utils/api'
 import { useState } from 'react'
 
 const Question = () => {
   const [value, setValue] = useState('')
-  const onChange = (e) => {
-    e.preventDefault()
-
-    //
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState()
+  const onChange = (e: any) => {
+    setValue(e.target.value)
   }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    setLoading(true)
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setValue('')
+    setLoading(false)
+  }
+
   return (
     <div>
-      <form action="">
+      <form className="flex gap-4" onSubmit={handleSubmit}>
         <input
-          onChange={onChange}
+          disabled={loading}
           value={value}
           type="text"
+          onChange={onChange}
           placeholder="Ask a question"
-          className="border border-black/20 px-4 py-2 text-lg  rounded-lg"
+          className="border border-black/20 px-4 py-2 text-lg  rounded-lg w-full"
         />
         <button
+          disabled={loading}
           type="submit"
-          className="bg-blue-400 px-4 py-2 rounded-lg text-lg"
+          className="bg-blue-400 px-4 py-2 rounded-lg text-lg w-40"
         >
           Ask
         </button>
       </form>
+      {loading && <div>...loading</div>}
+      {response && <div>{response}</div>}
     </div>
   )
 }
